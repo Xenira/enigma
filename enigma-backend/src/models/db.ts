9ccf5b09270c65bb0162cecc8a7de9af3023bc33
Lasse Sprengel <l.sprengel@fme.de>
2021-04-07 23:34:00 +0200

    Changed to database url

--- enigma-backend/src/models/db.ts
+++ enigma-backend/src/models/db.ts
@@ -3,14 +3,7 @@ import { init } from './user.model';
 
 const config: Knex.Config = {
 	client: 'pg',
-	connection: {
-		host: process.env.PG_HOST,
-		user: process.env.PG_USER,
-		password: process.env.PG_PASSWORD,
-		database: process.env.PG_DATABASE,
-	},
-	// connection:
-	// 	'user=postgres;Password=mysecretpassword;Server=postgres;Port=5432;Database=postgres;Pooling=true', //process.env.PG_CONNECTION_STRING,
+	connection: process.env.DATABASE_URL,
 };
 
 const knexInstance = knex(config);
